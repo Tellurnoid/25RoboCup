052e0ecf5b9d8a99f130ce6cc634f1ae9594cf96
tellurnoid <tellurnoid@tellurnoid.desktop>
2025-10-26 20:53:14 +0900

    withYata.9.26(＋コメント整理)

--- ESP32forUnitV/GPIO16,17/testWithYata.9.26/testWithYata.9.26.ino
+++ ESP32forUnitV/GPIO16,17/testWithYata.9.26/testWithYata.9.26.ino
@@ -1,6 +1,4 @@
-#include <Arduino.h>
-
-
+//Bluetoothデバッグ用
 #include "BluetoothSerial.h"
 #define USE_NAME  // Comment this to use MAC address instead of a slaveName
 BluetoothSerial SerialBT;
@@ -10,23 +8,20 @@ String slaveName = "ESP32-BT-Slave";  // Change this to reflect the real name of
 String MACadd = "AA:BB:CC:11:22:33";                        // This only for printing
 uint8_t address[6] = {0xAA, 0xBB, 0xCC, 0x11, 0x22, 0x33};  // Change this to reflect real MAC address of your slave BT device
 #endif
-String myName = "ESP32-BT-Master";
+String BTName = "ESP32-BT-Master";
 
 
 
-HardwareSerial &mySerial = Serial2; // UART2を使用する場合
-int motorSpeed= 120;
+//ヤタガラスの設定(136行まで)-----------------------
 #define INA1 33
 #define INA2 32
-
 #define INB1 26
 #define INB2 25
-
 #define INC1 27
 #define INC2 14
+#define IND1 13
+#define IND2 4
 
-#define IND1 13//5
-#define IND2 4//12
 void straight(){
   ledcWrite(INA1,motorSpeed);
   ledcWrite(INA2,0);
@@ -138,13 +133,19 @@ void stop(){
   ledcWrite(IND1,0);
   ledcWrite(IND2,0);
 }
-const int freq = 550;       // 周波数 Hz
-const int resolution = 8;    // 8ビット解像度（0〜motorSpeed）
+//-----------------------------------------------------
+
+
+const int freq = 550;        //pwm 周波数 Hz
+const int resolution = 8;    //pwm 8ビット解像度（0〜motorSpeed）
+HardwareSerial &mySerial = Serial2; // UART2を使用
+int motorSpeed= 120;//pwmの最大値(フルパワー:255)
 
 void setup() {
-  Serial.begin(115200);      // デバッグ用
-  SerialBT.begin(myName, true);
-  mySerial.begin(115200, SERIAL_8N1, 16, 17); // RX=16, TX=17 など適宜設定
+  Serial.begin(115200); 
+  SerialBT.begin(BTName, true);
+  mySerial.begin(115200, SERIAL_8N1, 16, 17); // RX=16, TX=17
+  //モーター
   ledcAttach(INA1, freq, resolution);   
   ledcAttach(INA2, freq, resolution);
   ledcAttach(INB1, freq, resolution);   
@@ -153,64 +154,64 @@ void setup() {
   ledcAttach(INC2, freq, resolution);
   ledcAttach(IND1, freq, resolution);   
   ledcAttach(IND2, freq, resolution);
-   stop();
-
-   ledcWrite(INB1,motorSpeed);
-   ledcWrite(INB2,0);
-   ledcWrite(INC1,motorSpeed);
-   ledcWrite(INC2,0);
-   delay(2000);
+  stop();
 }
-int Power;
-int guess;//0:前を見る 1:左にあるかも 2:右にあるかも
+
+
+int Power;//距離からpwmの出力値をおおよそ決めるための変数
+int guess = 0;//見失ったときの判断基準(0:前を見る 1:最後に見たのが左 2:最後に見たのが右)
+
 void loop() {
-  // データが6バイト以上あればブロブ情報を読む
+  // データが6バイト以上あればブロブ情報を読む(x座標,y座標,面積の順で計6バイト受信)
   if (mySerial.available() >= 2) {
     // まず2バイト読み込む
     uint8_t buf[6];
     mySerial.readBytes(buf, 2);
     int first = (buf[0] << 8) | buf[1];
 
-      if (first == 0xFFFF) {// ブロブなし
-        Power=90;
-        if(guess==0){stop();}
-        else if(guess==1){turnLeft();delay(200);guess=0;}
-        else{turnRight();delay(500);guess=0;}
+      if (first == 0xFFFF) {// ブロブなし(青色未検出の信号)
+        Power=90; //回転するときは遅め
+        if(guess==0){
+          stop();
+        }
+        else if(guess==1)
+        {
+          turnLeft();
+          delay(200);
+          guess=0;
+        }
+        else{//guess==2
+          turnRight();
+          delay(500);
+          guess=0;
+        }
         Serial.println("No blob detected");
       } 
       else 
       {
-      // ブロブあり、残り4バイトも読む
+        // ブロブあり(青検出)、残り4バイトも読む
         mySerial.readBytes(buf + 2, 4);
-        int cx     = first;
-        int cy     = (buf[2] << 8) | buf[3];
-        int pixels = (buf[4] << 8) | buf[5];
-        int paraCx = 160-cx;
-        int paraMotor1,paraMotor2,paraMotor3,paraMotor4;
-      
-        if(pixels > 8000 ){
-          Power = 0;
-        }
+        int cx     = first;                 //x座標
+        int cy     = (buf[2] << 8) | buf[3];//y座標
+        int pixels = (buf[4] << 8) | buf[5];//面積    (面積の求め方(ピクセル数を数えるor四角形で囲った面積)はunitV側のコードで設定)
+        int paraCx = 160-cx;                //x座標の、中央からの距離(右に行くと負、左に行くと正)
+        int paraMotor1,paraMotor2,paraMotor3,paraMotor4;//Powerの値とcxを考慮した各モーターの出力値
+
+        if(pixels > 8000){
+          Power = 0;//近すぎたら出力0
+         }
         else
         {
-          Power = motorSpeed - (pixels-100)*motorSpeed/9900; 
+          Power = motorSpeed - (pixels-100)*motorSpeed/9900;//離れるほど速く 
         }
-       // デバッグ出力
-        Serial.print("cx: "); Serial.print(cx);
-        Serial.print(", cy: "); Serial.print(cy);
-        Serial.print(", pixels: "); Serial.print(pixels);
-        Serial.print(", Power: "); Serial.print(Power);
-        Serial.print(", guess: "); Serial.print(guess);
-        
-       // if(pixels<1000){
-        if(abs(paraCx)<25){
+
+        if(abs(paraCx)<25){//正面に青
           straight();
           guess=0;
         }
-        //else if(cx < 20){guess=1;}
-        //else if(cx > 295){guess=2;}
-        else if(paraCx < 0){
+        else if(paraCx < 0){//右側に青
           guess=2;
+          //中央に近いほど前進、右にあるほどスライド
           paraMotor1 = Power+(Power*paraCx/160);
           paraMotor2 = -1*Power*paraCx/160;
           ledcWrite(INA1,paraMotor1);
@@ -222,10 +223,9 @@ void loop() {
           ledcWrite(IND1,Power);
           ledcWrite(IND2,0);
         }
-        else if(paraCx > 0){
+        else if(paraCx > 0){//左側に青
           guess=1;
-          //paraMotor1 = Power*(1-paraCx/160);
-          //paraMotor2 = Power-Power*(1-paraCx/160);
+          //中央に近いほど前進、左にあるほどスライド
           paraMotor3 = -1*Power*paraCx/160;
           paraMotor4 = Power+(Power*paraCx/160);
           ledcWrite(INA1,Power);
@@ -237,38 +237,13 @@ void loop() {
           ledcWrite(IND1,paraMotor3);
           ledcWrite(IND2,paraMotor4);      
         }
+
+        Serial.print("cx: "); Serial.print(cx);
+        Serial.print(", cy: "); Serial.print(cy);
+        Serial.print(", area: "); Serial.print(pixels);
+        Serial.print(", Power: "); Serial.print(Power);
+        Serial.print(", guess: "); Serial.print(guess);
         Serial.print(", paraCx:");Serial.print(paraCx);
-        Serial.print(",paraMotor1:");Serial.print(paraMotor1);
-        Serial.print(",paraMotor2:");Serial.println(paraMotor2);
-
-
-        SerialBT.print("cx: "); SerialBT.print(cx);
-        SerialBT.print(", area: "); SerialBT.print(pixels);
-        SerialBT.print(", Power: "); SerialBT.println(Power);;
       }
-/*    
-    //青未検出
-    if(cx > 320){
-      stop();
-    }
-    //右に物体あり
-    else if( (cx-160) > 90){
-      turnRight();
-    }
-    else if( (cx-160) > 30){
-      slideRight();
-    }
-
-    //左に物体あり
-    else if( (cx-160) < -90){
-      turnLeft();
-    }
-    else if( (cx-160) < 30){
-      slideLeft();
-    }
-    else{
-      straight();
-    }
-*/
   }
 }
--- UnitV/9.26青検出+UART/main.py
+++ UnitV/9.26青検出+UART/main.py
@@ -1,22 +1,20 @@
-import sensor, image, time
+import sensor, image, time, struct
 from fpioa_manager import fm
 from machine import UART
 
-import struct
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565) #16Bit color
 sensor.set_framesize(sensor.QVGA)   #320x240
 sensor.skip_frames(time = 2000)     #2秒間、カメラが安定するのを待つ。露出・ホワイトバランス調整のため
 clock = time.clock()                #fps計測などに関わる
 
-# K210のIO24=TX, IO25=RXにUART1を割り当て
+# K210のIO34=TX, IO35=RXにUART1を割り当て
 fm.register(34, fm.fpioa.UART1_TX)
 fm.register(35, fm.fpioa.UART1_RX)
 # UART1を初期化
 uart1 = UART(UART.UART1, 115200, timeout=1000, read_buf_len=4096)
 
 # LAB色空間で青色を検出
-#blue_threshold = [(33, 58, -128, 127, -128, -25)]
 blue_threshold = [(15, 60, -40, 80, -100, -40)]
 #(Lの最小,最大,  Aの最小,最大,  Bの最小,最大)  の順
 #L:黒か白か
@@ -28,8 +26,6 @@ cameraWidth  = 320
 cameraHeight = 240
 
 while True:
-    #"clock.tick()：フレーム時間計測を開始
-    #sensor.snapshot()：カメラ画像を1枚撮影して img に格納
     clock.tick()
     img = sensor.snapshot()
     #しきい値,ピクセル数の最小値,面積の最小値,隣り合った色をつなげる
@@ -41,17 +37,18 @@ while True:
             cx = b.cx()
             cy = b.cy()
             pixels = b.pixels()  # 面積（ピクセル数）
-            Rect = b.rect()
+            Rect = b.rect()      # 対象を囲んだ四角形
             x, y, w, h = Rect  # それぞれ取り出す
             area = w * h       # 面積計算
+
             #マーキング(四角形,十字)
             img.draw_rectangle(b.rect())
             img.draw_cross(cx, cy)
+
             data_bytes = struct.pack('>HHH', cx, cy, area)
             uart1.write(data_bytes)
     else:
         uart1.write(struct.pack('>H', 0xFFFF))
-        #time.sleep(1)
 
 
 
