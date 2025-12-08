89e27333bf2606617b58d223b68b13760eaef624
大地 <carbonoid333@gmail.com>
2025-12-08 19:43:30 +0900

    12.8

--- ラインセンサー/pinDebug/sketch_dec4a_copy_20251204200354/sketch_dec4a_copy_20251204200354.ino
+++ ラインセンサー/pinDebug/sketch_dec4a_copy_20251204200354/sketch_dec4a_copy_20251204200354.ino
@@ -1,22 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(A5, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A4, INPUT);
 }
 
 void loop() {
@@ -28,11 +28,11 @@ void loop() {
   Serial.print(", 3:");Serial.print(digitalRead(3));
   Serial.print(", 5:");Serial.print(digitalRead(5));
   Serial.print(", 4:");Serial.print(digitalRead(4));
-  Serial.print(", 7:");Serial.print(digitalRead(7));
+  Serial.print(", 7:");Serial.print(digitalRead(7));  
   Serial.print(", 6:");Serial.print(digitalRead(6));//OK
-  Serial.print(", 8:");Serial.print(digitalRead(8));
+  Serial.print(", 8:");Serial.print(!digitalRead(8));
   
-  Serial.print(", 9:");Serial.print(digitalRead(9));
+  Serial.print(", 9:");Serial.print(!digitalRead(9));
   Serial.print(", 11:");Serial.print(digitalRead(11));//OK
   Serial.print(", 12:");Serial.print(digitalRead(12));
   Serial.print(", A1:");Serial.print(digitalRead(A1));//OK
