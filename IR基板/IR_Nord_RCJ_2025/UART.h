dab6242cd675f6fa88d3eca5c38e32b6ae140bbf
大地 <carbonoid333@gmail.com>
2025-12-28 13:42:32 +0900

    ふぉるだ

---
+++ IR基板/IR_Nord_RCJ_2025/UART.h
@@ -0,0 +1,17 @@
+#ifndef UART_H
+#define UART_H
+
+struct ReadPacketData0 { int angleZ; };
+struct ReadPacketData1 { int angleZ; };
+struct ReadPacketData2 { int16_t dis1; int16_t dis2; int16_t dis3; int16_t dis4; };
+
+struct SendPacketData0 { int ball_angle; int ball_distance; int ball_max; int16_t dis1; int16_t dis2; int16_t dis3; int16_t dis4; };
+struct SendPacketData1 { int ball_angle; int goal_angle; };
+struct SendPacketData2 { int16_t serial_date; };
+
+void initUART();
+void UART();
+
+// extern int dis;
+
+#endif
